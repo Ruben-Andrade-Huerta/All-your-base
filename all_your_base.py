2dbead36e6051e29f8a033700794279fdbec48a7
Ruben-Andrade-Huerta <rubsan0503@gmail.com>
2025-03-20 21:23:57 -0600

    Base 2

--- all_your_base.py
+++ all_your_base.py
@@ -1,33 +1,25 @@
 def rebase(input_base, digits, output_base):
-    if input_base < 2 or output_base < 2:
-        raise ValueError("Bases must be at least 2.")
+    if input_base < 2:
+        raise ValueError("input base must be >= 2")
+    if output_base < 2:
+        raise ValueError("output base must be >= 2")
 
-    #Convertir a base decimal
+    # Convertir a base decimal
     valor_decimal = 0
     i = len(digits) - 1
-    print(i)
     for numero in digits:
         if numero < 0 or numero >= input_base:
-            raise ValueError("All digits must be valid for the input base.")
+            raise ValueError("all digits must satisfy 0 <= d < input base")
         valor_decimal += numero * (input_base ** i)
         i -= 1
-    
-    print(valor_decimal)
 
-    #Convertir a base deseada
+    # Caso especial para valor_decimal == 0
+    if valor_decimal == 0:
+        return [0]
+
+    # Convertir a base deseada
     output_digit = []
-    num_list = list(str(valor_decimal))
-
-    # for numero in num_list:
-    #     output_digit.append(int(numero) % output_base)
-    # return output_digit
-
     while valor_decimal > 0:
-        output_digits.append(valor_decimal % output_base)
-        valor_decimal //= output_base
-
-    return output_digit
-
-
-
-print(rebase(2, [1,0,1,0,1,0], 10))# [1]
+        output_digit.append(valor_decimal % output_base)
+        valor_decimal //= output_base  
+    return output_digit[::-1]
